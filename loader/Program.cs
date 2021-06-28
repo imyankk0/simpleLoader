d629c4b65c2a109c0dc00445fd836e94f7e5651d
Morsy <78912114+Morsy0@users.noreply.github.com>
2021-06-28 23:04:54 +0300

    Update Program.cs

--- loader/Program.cs
+++ loader/Program.cs
@@ -1,1 +1,57 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
 
+namespace loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String DnspySiken123 = "C:\\Users\\" + System.Environment.UserName + "\\Documents\\DnspySiken123.txt";
+            String crkonum = "C:\\Users\\" + System.Environment.UserName + "\\AppData\\Roaming\\.craftrise\\versions\\RiseClient_1.8.9\\RiseClient_1.8.9.jar";
+            String dnspynelaorospucocugu = "C:\\Users\\" + System.Environment.UserName + "\\AppData\\Roaming\\dnSpy\\dnSpy.xml";
+            if (File.Exists(DnspySiken123)) // Banı yedi mal XDE
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("DnspySiken123");
+                Thread.Sleep(2000);
+                Environment.Exit(0);
+            }
+            if (File.Exists(dnspynelaorospucocugu)) // Ban hazırlanıyor
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Dnspy Detected");
+                using (FileStream fs = File.Create(DnspySiken123))
+                {
+                    byte[] author = new UTF8Encoding(true).GetBytes("DnspySiken123");
+                    fs.Write(author, 0, author.Length);
+                }
+                Environment.Exit(0);
+            }
+            Console.Title = "Vanus Client"; // Yazılımınızın ismini buraya yazın
+            String vanusyazi = @"
+██╗   ██╗ █████╗ ███╗   ██╗██╗   ██╗███████╗
+██║   ██║██╔══██╗████╗  ██║██║   ██║██╔════╝
+██║   ██║███████║██╔██╗ ██║██║   ██║███████╗     By Morsy#7331
+╚██╗ ██╔╝██╔══██║██║╚██╗██║██║   ██║╚════██║
+ ╚████╔╝ ██║  ██║██║ ╚████║╚██████╔╝███████║
+  ╚═══╝  ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
+                                            
+";                                                // buradaki gibi ASCII yazı oluşturmak isterseniz "https://patorjk.com/software/taag/" sitesine göz atın
+            Console.ForegroundColor = ConsoleColor.Red; // ASCII yazı rengi
+            Console.WriteLine(vanusyazi); // ASCII yazı çalıştırılıyor
+            Console.ForegroundColor = ConsoleColor.White; // sisteme giriş yapıldıktan sonraki yazıların rengi
+            if (File.Exists(crkonum)) // RiseClient.jar (CRAFTRISE) bulunduysa programa giriş yapılıyor bulunmadıysa kapatıyor
+            {
+                Console.WriteLine("Welcome to Vanus Client");
+            }
+            else
+            {
+                Console.WriteLine("CraftRise bulunamadı!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
